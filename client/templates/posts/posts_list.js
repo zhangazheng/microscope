57a6d21e53d5808f86ee7a3d3bd8a865eedaaee6
zhangzheng <lovezhang.zheng@gmail.com>
2015-04-08 04:20:46 -0700

    add collection Posts

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,17 +1,6 @@
-var postsData = [
-	{
-		title:'Introducing Telescope',
-		url:'http://www.baidu.com'
-	},
-	{
-		title:'Meteor',
-		url:'http://meteor.com'
-	},
-	{
-		title:'The Meteor Book',
-		url:'http://www.baidu.com'
-	}
-];
+
 Template.postsList.helpers({
-	posts:postsData
+	posts:function(){
+		return Posts.find();
+	}
 }); 
