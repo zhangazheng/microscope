30c3523a53025e730122f3b3cd22480adac1e840
zhangzheng <lovezhang.zheng@gmail.com>
2015-04-08 02:40:45 -0700

    postData

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,17 @@
+var postsData = [
+	{
+		title:'Introducing Telescope',
+		url:'http://www.baidu.com'
+	},
+	{
+		title:'Meteor',
+		url:'http://meteor.com'
+	},
+	{
+		title:'The Meteor Book',
+		url:'http://www.baidu.com'
+	}
+];
+Template.postsList.helpers({
+	posts:postsData
+}); 
