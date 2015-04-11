67da8faccf505acbda80d8c03870e3d835256620
zhangzheng <lovezhang.zheng@gmail.com>
2015-04-11 02:47:30 -0700

    postInsert

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,7 +6,12 @@ Template.postSubmit.events({
 			url: $(e.target).find('[name=url]').val(),
 			title:$(e.target).find('[name=title]').val()
 		};
-		post._id = Posts.insert(post);
-		Router.go('postPage',post);
+		Meteor.call('postInsert',post,function(error,result){
+			//显示错误信息并退出
+			if(error){
+				return alert(error.reason);
+			}
+			Router.go('postPage',{_id:result._id});
+		});
 	}
 });
