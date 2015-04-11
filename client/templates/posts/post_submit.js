019e9a9d7f3afd0d8d9a90c4b947c2b56ae096df
zhangzheng <lovezhang.zheng@gmail.com>
2015-04-11 06:27:22 -0700

    通过时间排序

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -11,6 +11,9 @@ Template.postSubmit.events({
 			if(error){
 				return alert(error.reason);
 			}
+			if(result.postExists){
+				alert('改链接已经存在');
+			}
 			Router.go('postPage',{_id:result._id});
 		});
 	}
