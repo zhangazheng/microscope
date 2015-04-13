576a891449f220b48c13551aa8cf918838f7aec2
zhangzheng <lovezhang.zheng@gmail.com>
2015-04-12 19:22:40 -0700

    友好的显示错误信息

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,31 @@
+/**
+ * Created by fitark on 15-4-11.
+ */
+Template.postEdit.events({
+    'submit form': function (e) {
+        e.preventDefault();
+
+        var currentPostId = this._id;
+        var postProperties = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        };
+        Posts.update(currentPostId, {$set: postProperties}, function (error) {
+            if (error) {
+                alert(error.reason);
+            } else {
+                Router.go('postPage', {_id: currentPostId});
+            }
+        });
+
+    },
+    'click .delete': function (e) {
+        e.preventDefault();
+
+        if (confirm('确定删除此帖子吗？')) {
+            var currentPostId = this._id;
+            Posts.remove(currentPostId);
+            Router.go('postsList');
+        }
+    }
+});
