9917262d2d1f0b87c80905b1dcd600575b7423d7
zhangzheng <lovezhang.zheng@gmail.com>
2015-04-13 04:50:37 -0700

    添加评论

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,49 @@
 if (Posts.find().count() === 0) {
-  Posts.insert({
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  });
+    var now = new Date().getTime();
 
-  Posts.insert({
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  });
+    var tomId = Meteor.users.insert({
+        profile:{name:'Tom Coleman'}
+    });
+    var tom = Meteor.users.findOne(tomId);
+    var sachaId = Meteor.users.insert({
+        profile:{name:'Sacha Greif'}
+    });
+    var sacha = Meteor.users.findOne(sachaId);
 
-  Posts.insert({
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  });
+    var telescopeId = Posts.insert({
+        title: 'Introducing Telescope',
+        userId: sacha._id,
+        author: sacha.profile.name,
+        url: 'http://sachagreif.com/introducing-telescope/',
+        submitted: new Date(now - 7 * 3600 * 1000)
+    });
+    Comments.insert({
+        postId: telescopeId,
+        userId: tom._id,
+        author: tom.profile.name,
+        submitted: new Date(now - 5 * 3600 * 1000),
+        body: 'Interesting project Sacha, can I get involved?'
+    });
+
+    Comments.insert({
+        postId: telescopeId,
+        userId: sacha._id,
+        author: sacha.profile.name,
+        submitted: new Date(now - 3 * 3600 * 1000),
+        body: 'You sure can Tom!'
+    });
+    Posts.insert({
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    });
+
+    Posts.insert({
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    });
+
+    Posts.insert({
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    });
 }
